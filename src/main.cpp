5ac204363f49b6d673272fc1487531ab0fc18008
Maitreya Kulkarni <kulkarnimaitreya1@gmail.com>
2026-01-10 23:48:52 +0530

    Add CALL and RET opcodes with call stack management

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,20 @@ enum class Opcode {
     MUL,
     DIV,
     MOD,
+    CALL,
+    RET,
     HALT
+};
+
+struct callFrame {
+    int returnIP;
+    int frameBase;
 };
 
 struct VM {
     int ip;
-    vector<int> st; // stack
+    vector<int> st; // operand stack
+    vector<callFrame> callst; //call stack
     vector<int> bc;
     VM(){ ip = 0; }
 };
@@ -31,7 +39,7 @@ int main (){
         (int)Opcode::PUSH, 3,
         (int)Opcode::ADD,
         (int)Opcode::HALT
-    };
+    }; //hard-coded bytecode for testing
 
     bool running = true;
     while (running){
@@ -104,6 +112,32 @@ int main (){
             case Opcode::HALT:{
                 running = false;
                 break;
+            }
+            case Opcode::CALL:{
+                callFrame cf;
+                cf.frameBase = vm.st.size();
+                cf.returnIP = vm.ip + 2;
+                vm.callst.push_back(cf);
+                vm.ip = vm.bc[vm.ip + 1];
+                continue;
+            }
+            case Opcode::RET:{
+                assert(!vm.callst.empty());
+                callFrame temp = vm.callst.back();
+                int base = temp.frameBase;
+                int retIP = temp.returnIP;
+
+                int t;
+                bool hasReturn = vm.st.size() > base;
+
+                // if a return value exists, push it back on TOS after cleanup:
+                if (hasReturn) { t = vm.st.back(); }
+                vm.st.resize(base);
+                if (hasReturn) vm.st.push_back(t); // operand stack cleanup
+
+                vm.callst.pop_back(); // call stack cleanup
+                vm.ip = retIP;
+                continue;
             }
             default:{
                 perror("Wrong opcode");
